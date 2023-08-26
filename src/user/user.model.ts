11c7daa83fe0cfe4005f1b90e026bf46afc822f4
Basil <ohuruogubasil@gmail.com>
2023-08-26 08:15:04 +0100

    added:user/auth/filters

--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -1,6 +1,5 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 
-
 @ObjectType()
 export class User {
   @Field()
@@ -11,7 +10,6 @@ export class User {
 
   @Field()
   email?: string;
-
   @Field({ nullable: true })
   bio?: string;
 
